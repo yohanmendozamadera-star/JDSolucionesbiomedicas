90fe18dc558c6964d17f110146f923185c36fc5d
Yohan Mendoza <yohan_mendoza@outlook.com>
2026-08-28 17:02:09 -0500

    Corregir compatibilidad de despliegue en Netlify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingRoot: path.resolve("."),
+  turbopack: {
+    root: path.resolve("."),
+  },
 };
 
 export default nextConfig;
